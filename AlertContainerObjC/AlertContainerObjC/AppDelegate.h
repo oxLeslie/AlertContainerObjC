68e0d10a6d2fc3de47aff6edc2fd38f4b0caa8f5
qiuzhen <qiuzhen1@xiaoniu66.com>
2018-06-29 14:00:54 +0800

    初始化工程

---
+++ AlertContainerObjC/AlertContainerObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlertContainerObjC
+//
+//  Created by xiaoniu on 2018/6/29.
+//  Copyright © 2018年 AAAma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
